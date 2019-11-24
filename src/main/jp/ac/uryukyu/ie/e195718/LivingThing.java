cbb36b690b2967ffa4c6f917b11e54dc79a90479
Tomoharu Yakabi <e195718@ie.u-ryukyu.ac.jp>
2019-11-24 17:51:01 +0900

    プログラミング２report3

--- src/main/jp/ac/uryukyu/ie/e195718/LivingThing.java
+++ src/main/jp/ac/uryukyu/ie/e195718/LivingThing.java
@@ -1,10 +1,10 @@
 package jp.ac.uryukyu.ie.e195718;
 
 public class LivingThing {
-    String name;
-    int hitPoint;
-    int attack;
-    boolean dead;
+    private String name;
+    private int hitPoint;
+    private int attack;
+    private boolean dead;
 
     public LivingThing(String name, int maximumHP, int attack){
         this.name = name;
@@ -12,13 +12,37 @@ public class LivingThing {
         this.attack = attack;
         dead = false;
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-    }
+        }
     public boolean isDead() {
         return dead;
     }
 
+    public void setDead(boolean dead){
+        this.dead = dead;
+    }
+
     public String getName(){
         return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAttack(){
+        return attack;
+    }
+
+    public void setAttack(int attack){
+        this.attack = attack;
+    }
+
+    public int getHitPoint() {
+        return hitPoint;
+    }
+
+    public void setHitPoint(int hitPoint) {
+        this.hitPoint = hitPoint;
     }
 
     public void attack(LivingThing opponent) {
